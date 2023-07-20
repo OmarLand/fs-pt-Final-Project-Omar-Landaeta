31ea84aa105499f2b12ce2e30557e893b49fee2b
OmarjLand <omar.land2010@gmail.com>
2023-07-20 19:26:48 +0200

    style: Fixed the style of products view, to center by flexbox

--- front/src/components/Products/styles.jsx
+++ front/src/components/Products/styles.jsx
@@ -2,7 +2,10 @@ import { styled } from 'styled-components';
 
 const DivProducts = styled.div`
 
-    font-size : 13px;
+    font-size : 14px;
+
+    display: flex;
+    justify-content: center;
   
     .head-text{
         text-shadow : 1px 1px 10px black;
